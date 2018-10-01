1d977c4572dd62c7f4df807752d5cc03728284b4
Brad Rice <bradrice1@gmail.com>
2018-10-01 15:37:04 -0400

    Completed module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>{{title}}</h1>
+    <div>
+        <h1>{{title}}</h1>
+        <ul v-for="book in books">
+            <li>{{book.title}}:{{book.author}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -7,7 +12,12 @@ export default {
     name: 'BookList',
     data() {
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Oursleves to Death', author: 'Neil Postman'}
+            ]
         }
     }
 }
@@ -17,5 +27,13 @@ export default {
 <style>
 h1, h2 {
     font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0
+}
+li {
+    display: block;
+    padding: 0 10px;
 }
 </style>
